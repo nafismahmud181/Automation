4abf4af8a0700348efea5ba94d72f7e3245fc324
nafis-mahmud <nafis@aidocbuilder.com>
2025-07-23 15:40:16 +0600

    Enhance logging configuration in pytest and logger utility for improved output and reporting

--- utils/logger.py
+++ utils/logger.py
@@ -6,9 +6,17 @@ def get_logger(name=__name__):
         logger.setLevel(logging.INFO)
         formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
 
+        # Console handler
         console_handler = logging.StreamHandler()
         console_handler.setFormatter(formatter)
-
         logger.addHandler(console_handler)
+
+        # File handler for HTML reports
+        file_handler = logging.FileHandler('test_logs.log')
+        file_handler.setFormatter(formatter)
+        logger.addHandler(file_handler)
+
+        # Make sure logs appear in pytest output
+        logger.propagate = True
 
     return logger
